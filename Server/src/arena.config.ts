487c80a79749620f4c0c14689a0485199bb86999
fortey <fortey@inbox.ru>
2022-07-27 18:59:25 +0700

    e

--- Server/src/arena.config.ts
+++ Server/src/arena.config.ts
@@ -33,6 +33,24 @@ export default Arena({
          * Read more: https://docs.colyseus.io/tools/monitor/
          */
         app.use("/colyseus", monitor());
+
+        var MongoClient = require('mongodb').MongoClient;
+        let url = "";
+        if (process.env.MONGODB_URI == undefined)
+            url = "mongodb+srv://6thlop:blaBla!!@cluster0.scvjk.mongodb.net/?retryWrites=true&w=majority";
+        else url = process.env.MONGODB_URI;
+
+        MongoClient.connect(url, function (err: any, db: {
+            db(arg0: string): any; close: () => void;
+        }) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.createCollection("customers", function (err: any, res: any) {
+                if (err) throw err;
+                console.log("Collection created!");
+                db.close();
+            });
+        });
     },
 
 
